5ae6e8a02a93b1c8985fcdec5814052294fbb9da
djo kangoma <djokangoma@gmail.com>
2025-04-08 11:57:10 +0200

    chore:creation of a stuff.js file in the routes folder, which will contain the routes for all my objects

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const mongoose = require('mongoose');
 
 const app = express();
+const stuffRoutes = require('./routes/stuff');
 
-const Thing = require("./models/thing");
-const thing = require("./models/thing");
 const port = 3000;
 
 mongoose.connect('mongodb+srv://djokangoma:kangoma7@cluster0.dbz7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -20,41 +19,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use('/api/stuff', stuffRoutes);
   
-  app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({error}))
-  });
-
-  app.get(`/api/stuff/:id`, (req,res,next) =>{
-    Thing.findOne({_id: req.params.id})
-    .then(thing=>res.status(200).json(thing))
-    .catch(error=>res.status(404).json({error}))
-  })
-
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    })
-    thing.save()
-    .then(()=>res.status(201).json({message: "objet enregistré"}))
-    .catch(error => res.status(400).json({error}))
-  });
-
-  app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
 app.listen(process.env.port || 3000)
 console.log("le serveur est en ecoute");
 
